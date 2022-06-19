56fb951271fc1698b3bdebf8dd056234982cf902
Abraham Oh <abeoh1@gmail.com>
2022-06-19 15:55:23 -0400

    TSOA + YUP (#2)
    * basic tsoa implementation
    
    restructured code to implement the tsoa library
    
    created basic get function for /recipe/:id that returns a static object
    
    moved interface into separate file
    
    created controller and service file
    
    created default tsoa routes file
    
    changed name of old app.ts file to old-app.ts; this file should not be used anymore, kept only for reference
    
    * nodemon/express touchup
    
    fixed nodemon not updating on file change
    
    updated express server to include extended option to remove deprecation warning
    
    * Yup Implementation
    
    setup yup to validate request body
    
    added invalid id check for put
    
    added proper success responses
    
    * Clean up
    
    removed unneeded dependency
    
    removed original app.ts
    
    * update git ignore
    
    removed swagger.json and routes.ts
    
    * code clean up
    
    moved dotenv configuration to app.ts
    
    removed unnecessary variables
    
    * schema update
    
    updated steps to represent an array, not an object
    
    * pr fixes
    
    moved some devdependencies to depencies
    
    changed a function to find a singular result

--- src/app.ts
+++ src/app.ts
@@ -1,134 +1,52 @@
-import express  from 'express';
-import { promises as fs } from 'fs';
-import mongoose, { Model, Schema } from 'mongoose';
-import * as dotenv from 'dotenv';
-
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-app.use(express.urlencoded());
+import express, {
+  Response as ExResponse,
+  Request as ExRequest,
+  NextFunction,
+} from "express";
+import path from "path";
+import { ValidateError } from "tsoa";
+import * as dotenv from "dotenv";
 
 dotenv.config();
-const url = process.env.DB_URI;
-    
-mongoose.connect(url);
-const db = mongoose.connection;
-db.on('error', (error) => {
-    console.log(error)
-});
-db.once('connected', () => {
-    console.log('Database Connected');
-});
 
-var recipeSchema = new Schema({
-    id: Number,
-    name: String,
-    thumbnail: String,
-    description: String,
-    link: [],
-    metadata: {},
-    ingredients: [],
-    steps: {},
-});
+type TsoaRoutesModule = {
+  RegisterRoutes: Function;
+};
 
-interface Recipe {
-    id: number;
-    name: string;
-    thumbnail: string;
-    description: string;
-    link: [];
-    metadata: {};
-    ingredients: [];
-    steps: {};
-}
+export const app = express();
 
-const recipeModel = mongoose.model('recipes', recipeSchema)
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-//get recipes
-app.get('/recipe', async function(req, res) {
-    try {
-        const recipeId = req.query.id;
-        const ingredSearch = req.query.search;
-        if (recipeId) {
-            const recipeInt = +recipeId;
-            const data = await recipeModel.find({id:`${recipeInt}`});
-            res.status(200).json(data)
-        }
-        if (ingredSearch) {
-            const data = await recipeModel.find({ingredients: {$regex: `(.*)${ingredSearch}(.*)`}});
-            res.status(200).json(data)
-        }
-    }
-    catch(error) {
-        res.status(500).json({message: error.message})
-    }
-});
+const { RegisterRoutes } = require(path.resolve(
+  process.cwd(),
+  "./dist/routes"
+)) as TsoaRoutesModule;
 
-//get all recipes
-app.get('/recipe/all', async function(req, res) {
-    try {
-        const data = await recipeModel.find();
-        res.status(200).json(data)
-    }
-    catch(error) {
-        res.status(500).json({message: error.message})
-    }
-});
+RegisterRoutes(app);
 
-//add a new recipe
-app.post('/recipe', async function(req, res) {
-    const data = new recipeModel(req.body);
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 
-    try {
-        const dataToSave = await data.save();
-        res.status(201).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
-    }
-});
-
-//delete a recipe by id
-app.delete('/recipe', async function(req, res) {
-    try {
-        const recipeId = req.query.id;
-        const recipeInt = +recipeId;
-        await recipeModel.deleteOne({ id: `${recipeInt}` });
-        res.status(202).send("Object deleted")
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
-    }
-});
-
-//update a recipe by id
-app.patch('/recipe', async function(req,res) {
-    try{
-        const recipeId = req.query.id;
-        const recipeInt = +recipeId;
-        await recipeModel.updateOne({ id: `${recipeInt}`}, req.body);
-        res.status(200).json(req.body);
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
-    }
-});
-
-//get a list of recipes based on ingredients
-app.get('/recipe/search', async function(req, res) {
-    try{
-        console.log(req.query.search)
-        const recipeIngred = req.query.search;
-        
-        const data = await recipeModel.find({ingredients: {$regex: `(.*)${recipeIngred}(.*)`}});
-        res.status(200).json(data)
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
-    }
-});
-
-app.listen(port, () => {
-    console.log(`recipe application is running on port ${port}.`)
+  next();
 });
---
+++ src/recipeService.ts
@@ -0,0 +1,94 @@
+import { Recipe } from "./recipe";
+import mongoose, { Schema } from "mongoose";
+
+export type RecipeCreationParams = Pick<
+  Recipe,
+  | "id"
+  | "name"
+  | "thumbnail"
+  | "description"
+  | "link"
+  | "metadata"
+  | "ingredients"
+  | "steps"
+>;
+
+const url = process.env.DB_URI;
+
+mongoose.connect(url);
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.log(error);
+});
+db.once("connected", () => {
+  console.log("Database Connected");
+});
+
+var recipeSchema = new Schema({
+  id: Number,
+  name: String,
+  thumbnail: String,
+  description: String,
+  link: [],
+  metadata: {},
+  ingredients: [],
+  steps: [],
+});
+
+const recipeModel = mongoose.model("recipes", recipeSchema);
+
+export async function getRecipe(id: number) {
+  try {
+    return await recipeModel.findOne({ id: id });
+  } catch (error) {
+    return { message: error.message };
+  }
+}
+
+export async function getAllRecipe() {
+  try {
+    return await recipeModel.find();
+  } catch (error) {
+    return { message: error.message };
+  }
+}
+
+export async function searchRecipe(ingredient: string) {
+  try {
+    return await recipeModel.find({
+      ingredients: { $regex: `(.*)${ingredient}(.*)` },
+    });
+  } catch (error) {
+    return { message: error.message };
+  }
+}
+
+export async function createRecipe(recipeBody: Recipe) {
+  try {
+    const data = new recipeModel(recipeBody);
+    const dataToSave = await data.save();
+    return dataToSave;
+  } catch (error) {
+    return { message: error.message };
+  }
+}
+
+export async function deleteRecipe(id: number) {
+  try {
+    await recipeModel.deleteOne({ id: id });
+  } catch (error) {
+    return { message: error.message };
+  }
+}
+
+export async function updateRecipe(id: number, recipeBody: Recipe) {
+  try {
+    const data = await recipeModel.find({ id: id });
+    if (data.length === 0) {
+      return { message: "This ID does not exist" };
+    }
+    await recipeModel.updateOne({ id: id }, recipeBody);
+  } catch (error) {
+    return { message: error.message };
+  }
+}
